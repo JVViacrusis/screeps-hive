e9641c581f12e3cfcc35ab7266625a5e6396b791
justinv <justinv@pop-os.localdomain>
2022-02-13 21:22:39 -0800

    main aborts when no spawns in room

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,11 @@
 var overseerMiners = require("overseerMiners");
 
 module.exports.loop = function(){
+    if(Object.keys(Game.spawns).length < 1){
+        console.log("No spawns in room; Aborting...");
+        return;
+    }
+
     var sources = Game.spawns["Spawn1"].room.find(FIND_SOURCES);
 
     var minerNames = [];
--- dist/workerMiner.js
+++ dist/workerMiner.js
@@ -7,5 +7,4 @@ var workerMiner = {
     }
 }
 
-
 module.exports = workerMiner;
